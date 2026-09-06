482b300bb38f9260e4d59e3e374377c98e7757c2
rudragowda <rudra@nuchange.com>
2026-09-06 15:41:27 +0530

    Relational Operators In Java

---
+++ Operators/RelationalOperators.java
@@ -0,0 +1,14 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+
+        System.out.println("x == y: "+ (x == y));
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x < y: " + (x < y));
+        System.out.println("x >= y: " + (x >= y));
+        System.out.println("x <= y: " + (x <= y));
+        System.out.println("x != y: " + (x != y));
+
+    }
+}
