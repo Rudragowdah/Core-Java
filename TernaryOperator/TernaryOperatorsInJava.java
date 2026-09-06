7af6500bfdad7fdfaeb46e30cc9647e262f3b6e6
rudragowda <rudra@nuchange.com>
2026-09-06 19:10:09 +0530

    Ternary Operators In Java

---
+++ TernaryOperator/TernaryOperatorsInJava.java
@@ -0,0 +1,12 @@
+public class TernaryOperatorsInJava {
+    public static void main(String[] args) {
+
+        int n = 5;
+        int result = 0;
+
+        result = n%2 == 0 ? 10 : 20;
+
+        System.out.println("Result: " + result);
+
+    }
+}
