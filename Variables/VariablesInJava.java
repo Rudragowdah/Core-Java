2bc00f4daac0335ecf53ec4749ed0e8b906026e4
rudragowda <rudra@nuchange.com>
2026-09-06 12:14:28 +0530

    Variables In Java

---
+++ Variables/VariablesInJava.java
@@ -0,0 +1,13 @@
+public class VariablesInJava {
+    public static void main(String[] args) {
+        //In java, a Variable is a named place in memory used to store data.
+        //The Values stored in variables can change while Program Runs.
+        int num1 = 3;
+        int num2 = 7;
+        int result = num1 + num2;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num1 + num2);
+        System.out.println(result);
+    }
+}
