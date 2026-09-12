fd178356fd7bc8f23a56d81977a8b895c0a294ae
rudragowda <rudra@nuchange.com>
2026-09-12 13:37:11 +0530

    Do While Loops In Java

---
+++ LoopingStatements/DoWhileLoopsInJava.java
@@ -0,0 +1,13 @@
+public class DoWhileLoopsInJava
+{
+    public static void main(String[] args)
+    {
+        int i = 6;
+
+        do
+        {
+            System.out.println("Hi " + i);
+            i++;
+        }  while(i <= 5);
+    }
+}
