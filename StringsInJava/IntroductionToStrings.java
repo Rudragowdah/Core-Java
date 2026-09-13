7e9e5c21ab63bd7a26fda37c08217ed94593e079
rudragowda <rudra@nuchange.com>
2026-09-13 14:26:30 +0530

    Strings In Java.

---
+++ StringsInJava/IntroductionToStrings.java
@@ -0,0 +1,7 @@
+public class IntroductionToStrings {
+    public static void main(String[] args) {
+//        String name = "Rudra"; // This is Mostly Used
+        String name = new String("Rudra"); // This is Rarely Used. Both Creates the Object for Strings. No Much Difference in these two ways
+        System.out.println("Hello " + name);
+    }
+}
